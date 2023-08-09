2db37b339adf71ff1e5d269a601939b4c5ff258f
PedroShinon <jp.101zeta@gmail.com>
2023-08-09 08:57:46 -0300

    to deploy/ email

--- resources/views/mail/compra-realizada.blade.php
+++ resources/views/mail/compra-realizada.blade.php
@@ -4,7 +4,7 @@
 Compra realizada por {{ $nome }}, no valor total de R$ {{ $precoDaCompra }}, 
 no endereço {{$endereco}} clique para visualizar mais.
 
-<x-mail::button :url="'https://microcell-app.vercel.app/admin/orders'">
+<x-mail::button :url="'https://microcell-app.vercel.app'">
 Ver Mais
 </x-mail::button>
 
